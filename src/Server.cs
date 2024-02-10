64bb0c01851dd924cabbd85809f6baca36e2313a
Renzo Baasdam <renzobaasdam@gmail.com>
2024-02-10 18:34:36 +0100

    feat: handle concurrent clients

--- src/Server.cs
+++ src/Server.cs
@@ -5,22 +5,41 @@ using System.Text;
 Console.WriteLine("Starting TcpListener");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = server.AcceptSocket();
-Console.WriteLine("Accepted Socket");
+
+int socketNumber = 0;
 while (true)
 {
-    // receive
-    var buffer = new byte[512];
-    socket.Receive(buffer);
+    var socket = await server.AcceptSocketAsync();
+    Console.WriteLine($"Accepted Socket #{socketNumber}");
+    Listen(socket, socketNumber);
+    ++socketNumber;
+}
 
-    // input bytes to string
-    var input = Encoding.UTF8.GetString(buffer);
+static async void Listen(Socket socket, int socketNumber)
+{
+    while (true)
+    {
+        try
+        {
+            // receive
+            var buffer = new byte[512];
+            await socket.ReceiveAsync(buffer, SocketFlags.None);
 
-    // output string to bytes
-    var output = "+PONG\r\n";
-    var outputBuffer = Encoding.UTF8.GetBytes(output);
+            // input bytes to string
+            var input = Encoding.UTF8.GetString(buffer);
 
-    // log and respond
-    Console.WriteLine(@$"Received: {input.Replace("\r\n", "\\r\\n")}. Response: {output.Replace("\r\n", "\\r\\n")}");
-    socket.Send(outputBuffer);
+            // output string to bytes
+            var output = "+PONG\r\n";
+            var outputBuffer = Encoding.UTF8.GetBytes(output);
+
+            // log and respond
+            Console.WriteLine(@$"Socket #{socketNumber}. Received: {input.ReplaceLineEndings("\\r\\n")}. Response: {output.Replace("\r\n", "\\r\\n")}");
+            await socket.SendAsync(outputBuffer, SocketFlags.None);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Caught exception: {ex}");
+            break;
+        }
+    }
 }
