aca0f7febccb1099259ad11d60fe951d18b95edb
Renzo Baasdam <renzobaasdam@gmail.com>
2024-02-10 17:45:14 +0100

    feat: use socket

--- src/Server.cs
+++ src/Server.cs
@@ -1,26 +1,26 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Starting TcpListener!");
+Console.WriteLine("Starting TcpListener");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
+var socket = server.AcceptSocket();
+Console.WriteLine("Accepted Socket");
 while (true)
 {
-    // listen
-    var client = server.AcceptTcpClient();
-    var stream = client.GetStream();
+    // receive
+    var buffer = new byte[512];
+    socket.Receive(buffer);
 
     // input bytes to string
-    var buffer = new byte[512];
-    stream.Read(buffer, 0, buffer.Length);
-    var input = System.Text.Encoding.UTF8.GetString(buffer);
+    var input = Encoding.UTF8.GetString(buffer);
 
     // output string to bytes
     var output = "+PONG\r\n";
-    var outputBuffer = System.Text.Encoding.UTF8.GetBytes(output);
+    var outputBuffer = Encoding.UTF8.GetBytes(output);
 
     // log and respond
     Console.WriteLine(@$"Received: {input.Replace("\r\n", "\\r\\n")}. Response: {output.Replace("\r\n", "\\r\\n")}");
-    stream.Write(outputBuffer);
-    client.Close();
+    socket.Send(outputBuffer);
 }
