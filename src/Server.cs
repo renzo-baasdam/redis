48095c59b933cf546b1bdca7ea6b7fc01046b915
Renzo Baasdam <renzobaasdam@gmail.com>
2024-02-11 22:07:04 +0100

    feat: respond to echo

--- src/Server.cs
+++ src/Server.cs
@@ -29,7 +29,7 @@ static async void Listen(Socket socket, int socketNumber)
             var input = Encoding.UTF8.GetString(buffer);
 
             // output string to bytes
-            var output = "+PONG\r\n";
+            var output = Response(input);
             var outputBuffer = Encoding.UTF8.GetBytes(output);
 
             // log and respond
@@ -42,4 +42,26 @@ static async void Listen(Socket socket, int socketNumber)
             break;
         }
     }
+}
+
+static string Response(string input)
+{
+    var lines = input.Split("\r\n");
+    if (lines.Length > 0)
+    {
+        var arguments = lines[0];
+        if (arguments.Length > 1
+            && arguments[0] == '*'
+            && int.TryParse(arguments[1..], out var numberOfArguments))
+        {
+            var command = lines[2];
+            return command switch
+            {
+                "ECHO" => lines[4],
+                "PING" => "+PONG",
+                _ => "Unsupported request"
+            };
+        }
+    }
+    return "Unsupported request";
 }
