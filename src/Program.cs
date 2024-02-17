7a652e210db167a87ba1c9e1b35525068fd572b8
Renzo Baasdam <renzobaasdam@gmail.com>
2024-02-17 16:18:25 +0100

    refactor: auto cleanup

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        var config = new Dictionary<string,string>();
+        var config = new Dictionary<string, string>();
         for (int i = 0; i < args.Length; i += 2)
         {
             if (args[i] == RedisConfigKeys.Directory) config[RedisConfigKeys.Directory] = args[i + 1];
